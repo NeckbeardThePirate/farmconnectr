d339f06e6ab16ca64af71ddcddb6b35264b48c4d
judah helland <judah@hellands.com>
2023-12-15 15:01:57 -0800

    modifying cache and gitignore

--- farmconnectr/src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export var firebaseConfig = {
-    apiKey: "AIzaSyDT-oD3sZuAxmGXzlHBK6dC1fyKkwlBgK4",
-    authDomain: "farmconnectr.firebaseapp.com",
-    projectId: "farmconnectr",
-    storageBucket: "farmconnectr.appspot.com",
-    messagingSenderId: "388880470124",
-    appId: "1:388880470124:web:ae24d1447b9dcbc1628849",
-    measurementId: "G-HYX95953L8"
-  };
